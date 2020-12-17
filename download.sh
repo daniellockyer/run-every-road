30d7069d69f8482036b3e2bd1efb174e39346bca
Daniel Lockyer <hi@daniellockyer.com>
2020-12-17 17:19:02 +0000

    Refined initial data download types

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl 'https://overpass-api.de/api/interpreter' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-type: application/x-www-form-urlencoded; charset=UTF-8' --data-raw 'data=%5Btimeout%3A900%5D%5Bmaxsize%3A1073741824%5D%5Bbbox%3A53.916238%2C-1.16%2C54.011974%2C-1.0%5D%5Bout%3Ajson%5D%3B%0A(way%5Bhighway%5D%3B%20node(w)%3B)%3B%0Aout%20skel%3B'
+# https://wiki.openstreetmap.org/wiki/Key:highway
+
+curl 'https://overpass-api.de/api/interpreter' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-type: application/x-www-form-urlencoded; charset=UTF-8' --data-raw 'data=[timeout:900][maxsize:1073741824][bbox:53.916238,-1.16,54.011974,-1.0][out:json];(way[highway~"^(primary|secondary|tertiary|unclassified|residential|primary_link|secondary_link|tertiary_link|living_street|service|pedestrian|road)$"]; node(w););out skel;'
